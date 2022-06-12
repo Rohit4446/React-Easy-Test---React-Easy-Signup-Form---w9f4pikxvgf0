4be9ad7a4c7f72c7cadf97888a3fd8f9145a0d16
Rohit Bahuguna <90442893+Rohit4446@users.noreply.github.com>
2022-06-12 22:46:35 +0530

    ok

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,54 @@
-import React from 'react'
-import '../styles/App.css';
-
+import React, { useState } from "react";
+import "../styles/App.css";
+import signUpFormValidation from "../utils/validation";
 
 const App = () => {
-  
+  const formdata = {
+    name: "",
+    email: "",
+    password: ""
+  };
+  const [data, setData] = useState(formdata);
+
+  const changeName = (e) => {
+    setData({
+      name: e.target.value
+    });
+  };
+
+  const changeEmail = (e) => {
+    setData({
+      email: e.target.value
+    });
+  };
+  const changePassword = (e) => {
+    setData({
+      password: e.target.value
+    });
+  };
+
   return (
+    <>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          signUpFormValidation(data);
+        }}
+      >
+        <label>Name</label>
+        <input type="text" id="name" onChange={changeName} value={data.name} />
 
-  )
-}
-
+        <label>email</label>
+        <input type="email" id="email" onChange={changeEmail} />
+        <label>password</label>
+        <input type="password" id="password" onChange={changePassword} />
+        <label>checkbox</label>
+        <input type="checkbox" id="consent" />
+        <label>Sign up</label>
+        <input type="submit" value="Signup" />
+      </form>
+    </>
+  );
+};
 
 export default App;
